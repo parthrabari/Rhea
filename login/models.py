faf2ec963b7090990f213543d51d6d10fae05a1c
parthrabari <parthrabari@hotmail.com>
2017-01-28 00:36:55 +0530

    db setup complted

--- login/models.py
+++ login/models.py
@@ -5,7 +5,7 @@ class LoginData(models.Model):
     """docstring for ."""
     reg_no    = models.AutoField(primary_key=True)
     name      = models.CharField(max_length=50)
-    phone_no  = models.IntegerField(max_length=10)
+    phone_no  = models.IntegerField()
     email     = models.EmailField(max_length = 70)
     reg_type  = models.CharField(max_length=10)
-    no_of_tkt = models.IntegerField(max_length=10)
+    no_of_tkt = models.IntegerField()
